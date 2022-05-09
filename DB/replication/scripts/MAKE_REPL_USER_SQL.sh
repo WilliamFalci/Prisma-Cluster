5c12ae7f4f4f104689e882fcb245e57a9fb8afe1
WilliamFalci <falci.williampeter@gmail.com>
2022-05-09 15:12:00 +0200

    new priority on replication init structure

--- DB/replication/scripts/MAKE_REPL_USER_SQL.sh
+++ DB/replication/scripts/MAKE_REPL_USER_SQL.sh
@@ -2,5 +2,5 @@
 source ../../../CLI/env/.env
 source ../env/.env.replication
 
-echo "CREATE USER $POSTGRES_REPLICATION_USER WITH ENCRYPTED PASSWORD '$POSTGRES_REPLICATION_PASSWORD';" > ../init/00-Create_repl_user.sql
-echo "GRANT ALL PRIVILEGES ON DATABASE master TO $POSTGRES_REPLICATION_USER;" >> ../init/00-Create_repl_user.sql
+echo "CREATE USER $POSTGRES_REPLICATION_USER WITH ENCRYPTED PASSWORD '$POSTGRES_REPLICATION_PASSWORD';" > ../init/01-Create_repl_user.sql
+echo "GRANT ALL PRIVILEGES ON DATABASE master TO $POSTGRES_REPLICATION_USER;" >> ../init/01-Create_repl_user.sql
